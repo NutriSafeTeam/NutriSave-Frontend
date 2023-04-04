24e37102cb5c8326ed4a689b3e8aebeabbce9b82
RiumeCarlosA <riumecarlos@hotmail.com>
2023-04-04 12:31:40 +0100

    criando o escaner de código de barras

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './auth/auth.guard';
 import { LoginComponent } from './components/login/login.component';
 import { NavComponent } from './components/nav/nav.component';
+import { ScannerComponent } from './components/scanner/scanner.component';
 import { UsuarioListComponent } from './components/usuario/usuario-list/usuario-list.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent},
+  { path: 'scanner', component: ScannerComponent },
   { path: '', component: NavComponent },
   {
     path: 'usuarios', component: UsuarioListComponent, canActivate: [AuthGuard], children: [
